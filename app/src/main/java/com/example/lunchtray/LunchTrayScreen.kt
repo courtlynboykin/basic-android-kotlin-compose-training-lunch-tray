e08db4b8f564fd1848eb55da74f7423ce4b52bc4
Courtlyn Boykin <courtlynb@google.com>
2024-01-03 10:52:31 -0500

    added enum class to hold constants for each of the Lunch Tray app screens

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -15,6 +15,7 @@
  */
 package com.example.lunchtray
 
+import androidx.annotation.StringRes
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -23,8 +24,14 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lunchtray.ui.OrderViewModel
 
-// TODO: Screen enum
-
+// enum values that represent the screens in the app
+enum class Menus(@StringRes val title: Int){
+    Start(title = R.string.app_name),
+    Entree(title = R.string.choose_entree),
+    SideDish(title = R.string.choose_side_dish),
+    Accompaniment(title = R.string.choose_accompaniment),
+    Checkout(title = R.string.order_checkout)
+}
 // TODO: AppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
